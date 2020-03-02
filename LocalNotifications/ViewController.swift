5e5a0b09c1d8d9ced82b7a1c1670a0c7c22a7f46
Elena Kulakova <egeorgievska89@gmail.com>
2020-03-02 11:36:12 +0100

    Initial Commit

---
+++ LocalNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalNotifications
+//
+//  Created by Elena Kulakova on 2020-03-02.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
